9faa0b67f96c64ed68500e5bb59f95117b2bc9d0
anhtuan <anhtuan2704222@gmail.com>
2021-06-25 22:08:55 +0700

    Add project files.

---
+++ Lab03/Controllers/BookController.cs
@@ -0,0 +1,118 @@
+﻿using Lab03.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.Migrations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab03.Controllers
+{
+    public class BookController : Controller
+    {
+        // GET: Book
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult ListBook()
+        {
+            Model1 context = new Model1();
+            var listBook = context.Book.ToList();
+            return View(listBook);
+        }
+        [Authorize]
+        public ActionResult Buy(int id)
+        {
+            Model1 context = new Model1();
+            Book book = context.Book.SingleOrDefault(p => p.ID == id);
+            if (book == null)
+            {
+                return HttpNotFound();
+            }
+            return View(book);
+        }
+        public ActionResult Create()
+        {
+            
+            return View();
+        }
+        [Authorize]
+        [HttpPost]
+        public ActionResult Create(Book book)
+        {
+            try {
+                Model1 context = new Model1();
+                context.Book.AddOrUpdate(book);
+                context.SaveChanges();
+            }
+            catch(Exception ex)
+            {
+                var a = "";
+            }
+            
+
+            return RedirectToAction("ListBook");
+        }
+        
+        public ActionResult Edit(int id)
+        {
+            Model1 context = new Model1();
+            Book book = context.Book.SingleOrDefault(p => p.ID == id);
+            if (book == null)
+            {
+                return HttpNotFound();
+            }
+            return View(book);
+        }
+        [Authorize]
+        [HttpPost]
+        public ActionResult Edit(Book book)
+        {
+            Model1 context = new Model1();
+            Book bookupdate = context.Book.SingleOrDefault(p => p.ID == book.ID);
+            if (bookupdate != null)
+            {
+                context.Book.AddOrUpdate(book);
+                context.SaveChanges();
+            }
+            return RedirectToAction("ListBook");
+        }
+        public ActionResult Delete(int id)
+        {
+            Model1 context = new Model1();
+            Book book = context.Book.SingleOrDefault(p => p.ID==id);
+            if (book == null)
+            {
+                return HttpNotFound();
+            }
+            return View(book);
+        }
+        [Authorize]
+        [HttpPost]
+        //public ActionResult DeleteBook(int id)
+        //{
+        //    Model1 context = new Model1();
+        //    Book book = context.Book.SingleOrDefault(p => p.ID == id);
+        //    if (book != null)
+        //    {
+        //        context.Book.AddOrUpdate(book);
+        //        context.SaveChanges();
+        //    }
+        //    return RedirectToAction("ListBook");
+        //}
+        public ActionResult DeleteBook(int id)
+        {
+            Model1 context = new Model1();
+            Book book = context.Book.SingleOrDefault(p => p.ID == id);
+            if (book != null)
+            {
+                context.Book.Remove(book);
+                context.SaveChanges();
+
+            }
+            return RedirectToAction("ListBook");
+        }
+    }
+}
+    
---
+++ Lab03/Models/Book.cs
@@ -0,0 +1,37 @@
+﻿namespace Lab03.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("Book")]
+    public partial class Book
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+
+        [Required(ErrorMessage = "Không được để trống")]
+        public int ID { get; set; }
+
+        [Required(ErrorMessage = "Không được để trống")]
+        [StringLength(255, ErrorMessage = "Nhập quá ký tự")]
+        public string Title { get; set; }
+
+        [Required(ErrorMessage = "Không được để trống")]
+        [StringLength(255)]
+        public string Description { get; set; }
+
+        [Required(ErrorMessage = "Không được để trống")]
+        [StringLength(255)]
+        public string Author { get; set; }
+
+        [Required(ErrorMessage = "Không được để trống")]
+        [StringLength(255, ErrorMessage = "Nhập quá ký tự")]
+        public string Images { get; set; }
+
+        [Required(ErrorMessage = "Không được để trống")]
+        [Range(1000, 1000000, ErrorMessage = "tu 1k den 1M")]
+        public int? Price { get; set; }
+    }
+}
